e165feff0634d3e558da5b21a38461488405c0f4
nayan-bansal <nayan.7991@gmail.com>
2020-09-26 11:45:43 +0530

    Computation of Line Length

--- Line.java
+++ Line.java
@@ -1,9 +1,21 @@
 package line;
 
-public class User_Case3 {
-
+public class User_Case1 {
+	
 	public static void main (String[] args) {
 		
-		System.out.println("Welcome to Line Comparison Computation on Master Branch");
+		double x1,x2,y1,y2;
+		
+		x1 =   Math.floor(Math.random()*100);
+		x2 =   Math.floor(Math.random()*100);
+		y1 =   Math.floor(Math.random()*100);
+		y2 =   Math.floor(Math.random()*100);
+		
+		double length = 0;
+		
+		length  =   Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+		
+		System.out.println(length);
+		
 	}
 }
