44c65ba79efac4b4008b82f1369623c34fadcc55
nayan-bansal <nayan.7991@gmail.com>
2020-09-26 11:50:06 +0530

    Comparison of both Lines

--- Line.java
+++ Line.java
@@ -1,8 +1,10 @@
 package line;
 
-public class User_Case2 {
+public class User_Case3 {
 
 	public static void main (String[] args) {
+		
+		//Find Length of Line 1
 		
 		double x1,x2,y1,y2;
 		
@@ -13,7 +15,9 @@ public class User_Case2 {
 		
 		double length1 = 0;
 		
-		length1  = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+		length1  =   Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+		
+		// Find Length of Line 2
 		
 		double p1,p2,q1,q2;
 		
@@ -26,13 +30,19 @@ public class User_Case2 {
 		
 		length2  =   Math.sqrt(Math.pow(p1-p2, 2) + Math.pow(q1-q2, 2));
 		
+		
+		//Comparison of Two Lengths
+		
 		Double obj1 = new Double(length1);
 		Double obj2 = new Double(length2);
 		
-		if(obj1.equals(obj2) == true)
-			System.out.println("Lines are Equal");
-		else
-			System.out.println("Lines are not Equal");
+		//Program Output
 		
+		if(obj1.compareTo(obj2) == 1)
+		System.out.println("Line 1");
+		else if(obj1.compareTo(obj2) == -1)
+			System.out.println("Line 2");
+		else
+			System.out.println("Equal Lines");
 	}
 }
