e4dd269ba151f0cf6875e9b629200f2abfddd109
nayan-bansal <nayan.7991@gmail.com>
2020-09-26 11:52:12 +0530

    Program using object oriented programming

--- Line.java
+++ Line.java
@@ -1,11 +1,10 @@
 package line;
 
-public class User_Case3 {
-
-	public static void main (String[] args) {
-		
-		//Find Length of Line 1
-		
+public class User_Case4 {
+	
+	//Length of the Lines
+	public static double Length() {
+			
 		double x1,x2,y1,y2;
 		
 		x1 =   Math.floor(Math.random()*100);
@@ -13,36 +12,68 @@ public class User_Case3 {
 		y1 =   Math.floor(Math.random()*100);
 		y2 =   Math.floor(Math.random()*100);
 		
+		double length = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+		
+		return length;
+	}
+	
+	//Equality of the Lines
+	public static boolean Equality(double a, double b) {
+		
+		Double obj1 = new Double(a);
+		Double obj2 = new Double(b);
+		
+		
+		boolean equal  = obj1.equals(obj2);
+		
+		return equal;
+	}
+	
+	public static int Compare (double a, double b) {
+		
+		Double obj1 = new Double(a);
+		Double obj2 = new Double(b);
+		
+		int A  = obj1.compareTo(obj2);
+		return A;
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		//Computation of Length
+		
 		double length1 = 0;
 		
-		length1  =   Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+		length1  =  User_Case4.Length();
 		
-		// Find Length of Line 2
+		double length2 = 0;
 		
-		double p1,p2,q1,q2;
+		length2  = User_Case4.Length();
 		
-		p1 =   Math.floor(Math.random()*100);
-		p2 =   Math.floor(Math.random()*100);
-		q1 =   Math.floor(Math.random()*100);
-		q2 =   Math.floor(Math.random()*100);
+		//Check Equality
 		
-			double length2 = 0;
-		
-		length2  =   Math.sqrt(Math.pow(p1-p2, 2) + Math.pow(q1-q2, 2));
-		
-		
-		//Comparison of Two Lengths
-		
-		Double obj1 = new Double(length1);
-		Double obj2 = new Double(length2);
-		
-		//Program Output
-		
-		if(obj1.compareTo(obj2) == 1)
-		System.out.println("Line 1");
-		else if(obj1.compareTo(obj2) == -1)
-			System.out.println("Line 2");
+		if(Equality(length1, length2)== true)
+			System.out.println("Lines are Equal");
 		else
-			System.out.println("Equal Lines");
+			System.out.println("Lines are not Equal");
+		
+		//Comparison 
+		
+		switch(Compare(length1,length2)) {
+		case 1: 
+			System.out.println("Line 1 is greater than Line 2 with length: "+length1);
+			break;
+			
+		case -1: 
+			System.out.println("Line 2 is greater than Line 1 with length: "+length2);
+			break;
+		
+		default :
+			System.out.println("The Length of Equal lines is: "+length1);
+			
+		
+		
+		}	
 	}
 }
